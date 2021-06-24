4378969710a15fb54c0db873f3aa5da99e6747fd
Clement Siaffu <clement@shuledirect.org>
2021-06-24 18:35:03 +0300

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 const path = require('path')
 const routes = require('./routes')
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT || 8080
 app.get('/leta/:heartRate/:temperature',routes.leta)
 app.get("/pata",routes.pata)
 app.listen(PORT)
